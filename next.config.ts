ad9cce319b40ffb4bc337b18162cdfc46aa3f7eb
strbje <viktorrendov@yandex.ru>
2025-06-03 15:26:12 +0300

    Update next.config.ts
    добавил жесткий игнор eslint

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,11 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
