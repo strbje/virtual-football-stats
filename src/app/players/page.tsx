276bec46c43ac208076901b9fa4982ce875f71b5
strbje <viktorrendov@yandex.ru>
2025-06-30 22:43:31 +0300

    добавил console.log(rows);

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -21,6 +21,7 @@ export default async function Page() {
     LIMIT 50;
   `);
 
+  console.log(rows);
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Последние игроки (по матчам)</h1>
