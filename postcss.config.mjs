a4984e5b255188d5694824f843353616a1d75998
strbje <viktorrendov@yandex.ru>
2025-10-31 23:25:21 +0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,7 @@
 // postcss.config.mjs
-import tailwind from '@tailwindcss/postcss'
+import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 export default {
-  plugins: [
-    tailwind,
-    autoprefixer,
-  ],
+  plugins: [tailwindcss, autoprefixer],
 }
