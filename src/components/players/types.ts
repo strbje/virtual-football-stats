0da0a7562921fba831531668a55fe7435e087542
strbje <viktorrendov@yandex.ru>
2025-11-04 22:53:15 +0300

    Create types.ts

---
+++ src/components/players/types.ts
@@ -0,0 +1,4 @@
+// Единые типы, чтобы не расходились между файлами
+
+export type RoleItem = { label: string; value: number };     // для бара по ролям
+export type LeagueBucket = { label: string; pct: number };    // для бара по лигам
